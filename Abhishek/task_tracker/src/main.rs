b718e73a6312d90b80c51c2d11209916064051e5
Abhishek <abhishek@Technoidentity.com>
2026-09-10 16:52:31 +0530

    Add task tracker and new project

---
+++ Abhishek/task_tracker/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let app_name = "My Task Tracker";
+    println!("{}", app_name);
+    println!();
+    println!("1. Add a task");
+    println!("3. Mark a task complete");
+    println!("4.");
+}
