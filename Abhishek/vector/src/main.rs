0b7158ffb9c6b59b3ac71ea25001c44393eddb3b
Abhishek <abhishek@Technoidentity.com>
2026-09-18 14:39:15 +0530

    feat: add async function

---
+++ Abhishek/vector/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // println!("Hello, world!");
+    // let tasks : Vec<String> = Vec::new();
+    // This is a immutable vector 
+    // Immutable vectors are also possible to create 
+    greet();
+    show_menu();
+}
+
+fn greet(){
+    println!("Welcome!");
+
+}
+
+fn show_menu(){
+    println!("This is our Menu");
+    println!("Order something to eat");
+}
