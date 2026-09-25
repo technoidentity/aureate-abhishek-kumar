4bd8a8659457f98b232a1004113919a504cb19e6
Abhishek <abhishek@Technoidentity.com>
2026-09-26 01:56:15 +0530

    fix: added features

--- Abhishek/actix-web/src/main.rs
+++ Abhishek/actix-web/src/main.rs
@@ -1,5 +1,34 @@
 use actix_web::{App, HttpResponse, HttpServer, web};
+use serde::Deserialize;
 
+#[derive(Deserialize)]
+struct GCDParameters {
+    n: u64,
+    m: u64,
+}
+
+// Serialize: Converts your Rust structure structs or
+// enums into readable format such as XML or JSON
+
+// Deserialize does the vice versa 
+
+async fn post_gcd(form: web::Form<GCDParameters>) -> HttpResponse {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with 0 is 0 only. ")
+
+    }
+    let response = format!(
+        "The GCD of the numbers {} and {} is <b>{}</b>\n",
+        form.n,
+        form.m,
+        gcd(form.n, form.m),
+    );
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
+}
 
 #[actix_web::main]
 async fn main() {
